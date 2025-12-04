dfd4ce651828ea5ec7f0f751ceabbb3098f773c4
CarlosMoratinos5 <moratinos.carlos5@gmail.com>
2025-12-04 11:14:32 +0100

    add/middlewares

--- app.js
+++ app.js
@@ -3,6 +3,23 @@ const cowsay = require("cowsay");
 const app = express(); // Crear Servidor
 const port = 3000; // Puerto
 
+app.use(express.json());
+
+// Error 404
+const error404 = require('./middlewares/error404');
+
+//Morgan
+const morgan = require("./middlewares/morgan.js")
+
+// Configuración del logger con Morgan
+app.use(morgan(':method :url :status :param[id] - :response-time ms :body'));
+
+// Rutas Entries
+const entriesRoutes = require('./routes/entries.routes');
+app.use('/api/entries', entriesRoutes);
+
+app.use(error404);
+
 // Iniciar Servidor
 app.listen(port, () => {
 console.log(
